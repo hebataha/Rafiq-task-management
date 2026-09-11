cc5e1061e4ab8704a436949683b08e216015a822
heba taha <amltaha866@yahoo.om>
2026-09-11 14:40:24 +0300

    add files login - sign-up and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "login",
+        loadComponent: ()=>import('./features/auth/pages/login/login').then(m=>m.Login)
+    },
+        {
+        path: "sign-up",
+        loadComponent: ()=>import('./features/auth/pages/sign-up/sign-up').then(m=>m.SignUp)
+    },
+];
---
+++ src/app/features/auth/pages/sign-up/sign-up.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-sign-up',
+  styleUrl: './sign-up.css',
+  templateUrl: './sign-up.html',
+})
+export class SignUp {}

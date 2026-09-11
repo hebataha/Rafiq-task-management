4067c5fedfe7db4618923e75cfe2fa4f7c562da9
heba taha <amltaha866@yahoo.om>
2026-09-11 23:53:14 +0300

    add login main page to the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
     {
         path: "login",
         loadComponent: ()=>import('./features/auth/pages/login/login').then(m=>m.Login)

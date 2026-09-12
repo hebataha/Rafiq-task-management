9d5e80079fae9194d984b8806abad2d5a636e825
heba taha <amltaha866@yahoo.om>
2026-09-12 20:07:33 +0300

    add interface

---
+++ src/app/features/auth/models/login.ts
@@ -0,0 +1,5 @@
+export interface LoginModule {
+    email: string;
+    password: string;
+    rememberMe: boolean;
+}
--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { signal } from '@angular/core';
+import { email, form, FormField, minLength, required } from '@angular/forms/signals';
+import { RouterLink } from '@angular/router';
+import { LoginModule } from '../../models/login';
 
 @Component({
-  imports: [],
+  imports: [FormField],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
 export class Login {
- firstName = signal('John');
-  lastName = signal('Doe'); 
-
+  loginModel = signal<LoginModule>({
+    email: '',
+    password: '',
+    rememberMe: false,
+  })
+  
+  
+  loginForm = form(this.loginModel, (fields) => {
+    required(fields.email);
+    email(fields.email);
+    required(fields.password);
+    minLength(fields.password, 8);
+    
+  });
+ 
 }

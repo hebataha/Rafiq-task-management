297595a51fe5856f08ff3e87bdd54dc42e27836c
heba taha <amltaha866@yahoo.om>
2026-09-12 21:32:18 +0300

    add validation to email and pass

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -3,13 +3,15 @@ import { signal } from '@angular/core';
 import { email, form, FormField, minLength, required } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { LoginModule } from '../../models/login';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [FormField, RouterLink],
+  imports: [FormField, RouterLink ,JsonPipe],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
+  
 export class Login {
   loginModel = signal<LoginModule>({
     email: '',
@@ -20,10 +22,8 @@ export class Login {
   
   loginForm = form(this.loginModel, (fields) => {
     required(fields.email, {message:"email is required"});
-    email(fields.email);
-    required(fields.password ,{message:"password is required"});
-    minLength(fields.password, 8);
-    
+    email(fields.email , {message:"please enter a vaild email"});
+    required(fields.password ,{message:"password is required"});    
   });
  
 }

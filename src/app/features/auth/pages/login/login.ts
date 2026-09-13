3af432a7e6c969a733694f50226a2f12e7c6b6c8
heba taha <amltaha866@yahoo.om>
2026-09-13 13:14:29 +0300

    handle API LOGIN

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,46 +1,51 @@
 import { Component } from '@angular/core';
 import { signal } from '@angular/core';
 import { email, form, FormField, minLength, required } from '@angular/forms/signals';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LoginModule } from '../../models/login';
 import { JsonPipe } from '@angular/common';
 import { LoginApi } from '../../services/login';
 
 @Component({
-  imports: [FormField, RouterLink ,JsonPipe],
+  imports: [FormField, RouterLink, JsonPipe],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
-  
+
 export class Login {
-  constructor(private _LoginApi:LoginApi) {
-    
+  constructor(private _LoginApi: LoginApi , private _Router:Router) {
+
   }
+  apiError: string = "";
   loginModel = signal<LoginModule>({
     email: '',
     password: '',
     rememberMe: false,
   })
-  
+
   loginForm = form(this.loginModel, (fields) => {
-    required(fields.email, {message:"email is required"});
-    email(fields.email , {message:"please enter a vaild email"});
-    required(fields.password ,{message:"password is required"});    
+    required(fields.email, { message: "email is required" });
+    email(fields.email, { message: "please enter a vaild email" });
+    required(fields.password, { message: "password is required" });
   });
 
   login() {
-  alert("heba")
-  this._LoginApi.loginData().subscribe({
-    next: (res) => {
-      console.log(res);
+    console.log(this.loginModel().email);
+    console.log(this.loginModel().password);
+    this._LoginApi.loginData(this.loginModel().email, this.loginModel().password).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._Router.navigate(['/projects'])
+        
 
-     
-    },
-    error: (err) => {
-      console.log(err);
-    }
-  });
-}
- 
+
+      },
+      error: (err) => {
+        console.log(err);
+        this.apiError = err.error.msg;
+      }
+    });
+  }
+
 }
--- src/app/features/auth/services/login.ts
+++ src/app/features/auth/services/login.ts
@@ -7,12 +7,18 @@ export class LoginApi {
     private readonly apiUrl = "https://ewryvwlqqqvwbgmacgau.supabase.co";
 
  
-    loginData() {
+    loginData(email: string, password: string) {
     
         return this.http.post(this.apiUrl + "/auth/v1/token?grant_type=password", {
-            "email": "",
-            "password":"",
+            email,
+            password,
             
-        })
+        },
+    {
+      headers: {
+        apikey: 'sb_publishable_MUL_oO1sCf-c5NKi1as6Sg_42ECtX8U',
+        'Content-Type': 'application/json'
+      }
+    })
     }
 }

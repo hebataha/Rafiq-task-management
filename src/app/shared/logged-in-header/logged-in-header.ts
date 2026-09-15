84a66852b3b1455fdcf0695cbb3d726318e5164c
heba taha <amltaha866@yahoo.om>
2026-09-15 16:29:45 +0300

    add header logged-in styles

--- src/app/shared/logged-in-header/logged-in-header.ts
+++ src/app/shared/logged-in-header/logged-in-header.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-logged-in-header',
-  styleUrl: './logged-in-header.css',
+  styleUrl: './logged-in-header.scss',
   templateUrl: './logged-in-header.html',
 })
 export class LoggedInHeader {}

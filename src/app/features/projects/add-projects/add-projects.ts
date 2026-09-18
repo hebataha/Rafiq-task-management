c89926bac76870e9536138db6a2e83ecb2b79d86
heba taha <amltaha866@yahoo.om>
2026-09-18 18:27:42 +0300

    add maxlength to add project

--- src/app/features/projects/add-projects/add-projects.ts
+++ src/app/features/projects/add-projects/add-projects.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { AddProjectsModules } from './modules/add-projects';
+import { form, FormField, maxLength, minLength, required } from '@angular/forms/signals';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [JsonPipe, FormField],
   selector: 'app-add-projects',
   styleUrl: './add-projects.css',
   templateUrl: './add-projects.html',
 })
-export class AddProjects {}
+
+export class AddProjects {
+  addProject = signal<AddProjectsModules>({
+    title: '',
+    description: '',
+  })
+
+
+  projectForm = form(this.addProject, (fields) => {
+    required(fields.title, { message: "project title is required" });
+    minLength(fields.title, 3, { message: "Project title must be at least 3 characters" });
+    maxLength(fields.title, 100, { message: "Project title must be max characters 100" });
+
+    maxLength(fields.description, 500)
+  });
+
+}
---
+++ src/app/features/projects/add-projects/modules/add-projects.ts
@@ -0,0 +1,4 @@
+export interface AddProjectsModules {
+    title: string;
+    description: string;
+}

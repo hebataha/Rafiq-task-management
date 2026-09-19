fc01e1476c510576e976590c56f83818a63dcaa2
heba taha <amltaha866@yahoo.om>
2026-09-19 15:55:06 +0300

    change router in add project

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { AddProjects } from './add-projects/add-projects';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [AddProjects, RouterOutlet],
+  imports: [AddProjects, RouterOutlet,RouterLink],
   selector: 'app-projects',
   styleUrl: './projects.css',
   templateUrl: './projects.html',

4553896b496b9fb9ad0fcf0712a596954a241796
heba taha <amltaha866@yahoo.om>
2026-09-24 12:53:01 +0300

    handle visability menu

--- src/app/features/projects/project-details/project-details.ts
+++ src/app/features/projects/project-details/project-details.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectId } from '../services/project-id';
 @Component({
   imports: [],
   selector: 'app-project-details',
   styleUrl: './project-details.css',
   templateUrl: './project-details.html',
 })
-export class ProjectDetails {}
+export class ProjectDetails implements OnInit {
+  _ActivatedRoute = inject(ActivatedRoute);
+  _ProjectId = inject(ProjectId);
+  ngOnInit() {
+    const id = this._ActivatedRoute.snapshot.paramMap.get('id');
+
+    console.log("id in details", id);
+    if (id) {
+      this._ProjectId.id.set(id);
+    }
+  }
+}
--- src/app/shared/components/side-bar/side-bar.ts
+++ src/app/shared/components/side-bar/side-bar.ts
@@ -1,51 +1,92 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink
+} from '@angular/router';
+
+import { filter, map, startWith } from 'rxjs';
+
 import { AuthUserService } from '../../layouts/auth-user-service';
-import { Router, RouterLink } from '@angular/router';
 import { ToastService } from '../../services/toast';
 import { ProjectId } from '../../../features/projects/services/project-id';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   selector: 'app-side-bar',
   styleUrl: './side-bar.scss',
   templateUrl: './side-bar.html',
 })
 export class SideBar {
-  constructor(private _AuthUserService: AuthUserService, private _Router: Router, private _ToastService: ToastService, public _ProjectId:ProjectId) {
 
-  }
+  private _Router = inject(Router);
+  private _AuthUserService = inject(AuthUserService);
+  private _ToastService = inject(ToastService);
+  public _ProjectId = inject(ProjectId);
+
+  constructor(
+
+  ) {}
+
   isExpanded = false;
   textCollapse = false;
-  errMsg = "";
-  loadingState: boolean = false;
+  errMsg = '';
+  loadingState = false;
+
+  showProjectMenu = this._Router.events.pipe(
+
+    filter(
+      (event): event is NavigationEnd =>
+        event instanceof NavigationEnd
+    ),
+
+    startWith(null),
+
+    map(() =>
+      this._Router.url.startsWith('/project/')
+    )
+
+  );
+
   toggle(): void {
     this.isExpanded = !this.isExpanded;
   }
+
   sidebarCollapse() {
     this.textCollapse = !this.textCollapse;
   }
+
   logout() {
+    this.loadingState = true;
+
     this._AuthUserService.logout().subscribe({
+
       next: () => {
-        localStorage.removeItem("access_token"),
-          localStorage.removeItem("refresh_token"),
-          this._Router.navigate(['/login']),
-              this.loadingState = false;
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+
+        this.loadingState = false;
+
+        this._Router.navigate(['/login']);
 
         this._ToastService.show(
-          'logined out succefully',
-          "success"
-        )
-
+          'Logged out successfully',
+          'success'
+        );
       },
+
       error: (err) => {
-        console.log("we have an error", err.error),
-          this.loadingState = false;
+        console.log('we have an error', err.error);
+
+        this.loadingState = false;
+
         this._ToastService.show(
-          '404 error',
-          "error"
-        )
+          'Logout failed',
+          'error'
+        );
       }
-    })
+
+    });
   }
 }

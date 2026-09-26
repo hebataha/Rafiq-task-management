8102dcb9dc935bc5674b3001bc476fd04ac83f45
heba taha <amltaha866@yahoo.om>
2026-09-26 16:45:54 +0300

    loading header logged-in

--- src/app/shared/logged-in-header/logged-in-header.ts
+++ src/app/shared/logged-in-header/logged-in-header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthUserService } from '../layouts/auth-user-service';
 import { AuthUser } from '../layouts/auth-user';
 import { Router } from '@angular/router';
@@ -9,32 +9,41 @@ import { Router } from '@angular/router';
   styleUrl: './logged-in-header.scss',
   templateUrl: './logged-in-header.html',
 })
-export class LoggedInHeader {
+export class LoggedInHeader implements OnInit {
   userData: AuthUser | null = null;
-  constructor(private _AuthUserService: AuthUserService,private _Router:Router) {
+  loading: boolean = false;
+  constructor(private _AuthUserService: AuthUserService, private _Router: Router) {
+
+
+  }
+  ngOnInit(): void {
     this.getData()
   }
+
   getname(name?: string) {
     if (!name) return "";
     return name.trim()
-    .split(' ')
-    .filter(Boolean)
-    .map(word => word[0])
-    .join('')
-    .toUpperCase();
+      .split(' ')
+      .filter(Boolean)
+      .map(word => word[0])
+      .join('')
+      .toUpperCase();
 
-  
-}
+
+  }
 
   getData() {
+    this.loading = true;
 
     this._AuthUserService.getUserData().subscribe({
       next: (res: any) => {
         console.log(' user data', res.user_metadata);
         this.userData = res.user_metadata;
+        this.loading = false;
       },
       error: (err: any) => {
         console.log('user error', err);
+        this.loading = true
       }
 
     })
